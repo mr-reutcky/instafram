8853af4a3e3b85678c6b2aed08e9d5341a3fb959
Samuel Reutcky <sreutcky@gmail.com>
2025-02-20 13:47:40 -0600

    Registered Services and Repos

---
+++ instafram/Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using instafram.BLL;
+using instafram.Models;
+using System.Collections.Generic;
+
+namespace instafram.Controllers {
+    public class UserController : Controller {
+        private readonly UserService _userService;
+
+        public UserController(UserService userService) {
+            _userService = userService;
+        }
+        public IActionResult Index() {
+            List<User> users = _userService.GetUsers();
+            return View(users);
+        }
+    }
+}
--- instafram/Program.cs
+++ instafram/Program.cs
@@ -1,21 +1,44 @@
-namespace instafram
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
+using instafram.BLL;
+using instafram.Models;
+using instafram.DAL;
+using Microsoft.EntityFrameworkCore;
+
+namespace instafram {
+    public class Program {
+        public static void Main(string[] args) {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            // Register DbContext with the connection string from appsettings.json
+            builder.Services.AddDbContext<InstaframContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+            // Register Repositories and Services
+            builder.Services.AddTransient<UserRepository>();
+            builder.Services.AddTransient<PostRepository>();
+            builder.Services.AddTransient<CommentRepository>();
+            builder.Services.AddTransient<StoryRepository>();
+            builder.Services.AddTransient<FollowRepository>();
+            builder.Services.AddTransient<DmRepository>();
+            builder.Services.AddTransient<LikeRepository>();
+            builder.Services.AddTransient<StoryLikeRepository>();
+
+            builder.Services.AddTransient<UserService>();
+            builder.Services.AddTransient<PostService>();
+            builder.Services.AddTransient<CommentService>();
+            builder.Services.AddTransient<StoryService>();
+            builder.Services.AddTransient<FollowService>();
+            builder.Services.AddTransient<DmService>();
+            builder.Services.AddTransient<LikeService>();
+            builder.Services.AddTransient<StoryLikeService>();
+
+            // Add services to the container (MVC)
             builder.Services.AddControllersWithViews();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
+            // Configure the HTTP request pipeline
+            if (!app.Environment.IsDevelopment()) {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
