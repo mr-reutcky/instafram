ae8191b1221e884065d1e24df878281990c3cbc5
Creeford Sanchez <creewrk@gmail.com>
2025-02-20 10:34:28 -0800

    add-post-controller

---
+++ instafram/Controllers/PostController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using instafram.BLL;
+using instafram.DAL;
+using instafram.Models;
+
+namespace instafram.Controllers {
+    public class PostController : Controller {
+        private static PostRepository _postRepository = new PostRepository(new InstaframContext());
+        private static PostService _postService = new PostService(_postRepository);
+
+        public PostController() {
+        }
+        public IActionResult Index() {
+            return View(_postService.GetPosts());
+        }
+    }
+}
