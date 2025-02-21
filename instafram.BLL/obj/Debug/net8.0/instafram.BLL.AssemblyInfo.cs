4342514578d1d09046e27886d76da007370e7038
Samuel Reutcky <sreutcky@gmail.com>
2025-02-21 00:24:17 -0600

    Users and user page done and minor SQL tweaks

--- instafram.BLL/obj/Debug/net8.0/instafram.BLL.AssemblyInfo.cs
+++ instafram.BLL/obj/Debug/net8.0/instafram.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("instafram.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbc6e6c67dabbef06e713229956fbf2e5382e8e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8853af4a3e3b85678c6b2aed08e9d5341a3fb959")]
 [assembly: System.Reflection.AssemblyProductAttribute("instafram.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("instafram.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- instafram.Models/obj/Debug/net8.0/instafram.Models.AssemblyInfo.cs
+++ instafram.Models/obj/Debug/net8.0/instafram.Models.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("instafram.Models")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d70e4d75347f1ccc0ec267ccc4a1dd4740cf1b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8853af4a3e3b85678c6b2aed08e9d5341a3fb959")]
 [assembly: System.Reflection.AssemblyProductAttribute("instafram.Models")]
 [assembly: System.Reflection.AssemblyTitleAttribute("instafram.Models")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
