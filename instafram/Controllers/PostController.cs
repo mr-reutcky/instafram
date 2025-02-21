386e744cf113395735fa2f3814fc8832c00e08d1
Creeford Sanchez <creewrk@gmail.com>
2025-02-21 04:03:58 -0800

    post-comment-add/view

--- instafram/Controllers/PostController.cs
+++ instafram/Controllers/PostController.cs
@@ -7,13 +7,53 @@ namespace instafram.Controllers {
     public class PostController : Controller {
 
         private readonly PostService _postService;
+        private readonly CommentService _commentService;
+        private int _commentId;
 
-        public PostController(PostService postService) {
+        public PostController(PostService postService, CommentService commentService) {
             _postService = postService;
+            _commentService = commentService;
+            _commentId = LastCommentID();
         }
         public IActionResult Index() {
             List<Post> post = _postService.GetPosts();
             return View(post);
         }
+
+        public IActionResult Comment(int Id) {
+            ViewBag.PostID = Id;
+            List<Comment> comment = _commentService.GetComments();
+            List<Comment> postComments = comment.Where(c => c.PostId == Id).ToList();
+
+            return View(postComments);
+        }
+
+        [HttpGet]
+        public IActionResult CommentForm(int Id) {
+            ViewBag.PlayerId = 1;
+            ViewBag.PostID = Id;
+
+            return View();
+        }
+
+       [HttpPost]
+        public IActionResult CommentForm(Comment comment) {
+            if (ModelState.IsValid) {
+               // comment.CommentId = _commentId++;
+                _commentService.CreateComment(comment);
+                return Redirect("/Post/Index");
+            }
+            return View(comment);
+        }
+        
+        private int LastCommentID() {
+            int lastId = _commentService.GetComments()
+                .OrderByDescending(c => c.CommentId) 
+                .Select(c => c.CommentId)
+                .FirstOrDefault();
+
+            return lastId + 1; 
+        }
+
     }
 }
